d2ad47bb483892bc55ccf4345cccbcdf89379c7d
larissa <hermanns_larissa@web.de>
2024-05-20 17:52:16 +0200

    First Commit

---
+++ install.py
@@ -0,0 +1,10 @@
+
+
+
+import subprocess
+
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "git+https://github.com/saezlab/decoupler-py"])
+
+#anndata_object.h5ad for decoupler
