89150cd0377f38a478dbc901c2584108a94899c1
larissa <larissa.hermanns@rwth-aachen.de>
2024-06-04 17:41:52 +0200

    first code

--- input_read.py
+++ input_read.py
@@ -4,9 +4,3 @@
 #' @param arguments_list list of user defined arguments
 #' @return val_arguments validated list of arguments
 #' @export
-
-def validate_input_arguments (arguments_list,**kwargs):
-    if out_path in arguments_list is None:
-        print("Please provide an output path")
-    else: 
-        if 
